61920c2d4e0fc46728c76f7e560f4ef31a9a91f6
Seb Carss <seb.carss@gmail.com>
2017-01-29 12:51:40 +0000

    Initial setup of app

---
+++ Mob-Fantasy/src/main/java/com/scarss/App.java
@@ -0,0 +1,15 @@
+package com.scarss;
+
+/**
+ * Mob Fantasy - A game to give a group of people the experience of programming in a mob. This is a game that
+ * should be played with 5 people, non of whom need be developers. The point of this exercise is to allow 
+ * people to get a feel for the benefits of pair programming through programming as a mob. 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Mob Fantasy!" );
+    }
+}
