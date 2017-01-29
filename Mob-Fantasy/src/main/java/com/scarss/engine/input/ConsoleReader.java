5bb255912dc8ad9c3d5fa9b0fa081a8f33d59b4e
Seb Carss <seb.carss@gmail.com>
2017-01-29 18:49:03 +0000

    Add > at the start of the user input to signal entry

--- Mob-Fantasy/src/main/java/com/scarss/engine/input/ConsoleReader.java
+++ Mob-Fantasy/src/main/java/com/scarss/engine/input/ConsoleReader.java
@@ -2,11 +2,13 @@ package com.scarss.engine.input;
 
 public class ConsoleReader implements UserInputReadable {
 
+	private static final String CONSOLE_START_CHAR = "> ";
+
 	/* (non-Javadoc)
 	 * @see com.scarss.engine.input.UserInputReadable#getUserInput()
 	 */
 	@Override
 	public String getUserInput() {
-		return System.console().readLine();
+		return System.console().readLine(CONSOLE_START_CHAR);
 	}
 }
